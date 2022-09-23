e6d162fffd65ee3eef42dab14b0c4ec3c3d91942
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-23 18:25:46 +0300

    Conditions(class binding)

--- 7-conditions/app.js
+++ 7-conditions/app.js
@@ -5,4 +5,12 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  computed: {
+    counterBoxClass() {
+      return {
+        "bg-success": this.counter > 0,
+        "bg-danger text-white": this.counter < 0,
+      };
+    },
+  },
 }).mount("#app");
