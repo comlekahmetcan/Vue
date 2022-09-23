bc13cce9a1d61bde56b1b3dd8e5a0cb409333589
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-23 19:14:00 +0300

    Dynamic Attribute Binding

--- 8-loops/app.js
+++ 8-loops/app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      todoList: ["Todo1", "Todo2", "Todo3", "Todo4"],
+      todoList: [
+        { id: 1, text: "Vue Bootcamp Hafta 1", completed: false },
+        { id: 2, text: "Vue Bootcamp Hafta 2", completed: false },
+        { id: 3, text: "Vue Bootcamp Hafta 3", completed: false },
+        { id: 4, text: "Vue Bootcamp Hafta 4", completed: false },
+        { id: 5, text: "Vue Bootcamp Hafta 5", completed: false },
+        { id: 6, text: "Vue Bootcamp Hafta 6", completed: false },
+        { id: 7, text: "Vue Bootcamp Hafta 7", completed: false },
+      ],
     };
   },
   methods: {
