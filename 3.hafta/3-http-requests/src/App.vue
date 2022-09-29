b1cef2fbf208048062c32ce68f819b76539aeed6
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-29 15:53:33 +0300

    Axios

---
+++ 3.hafta/3-http-requests/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <hr />
+    <div class="my-2">
+      <input type="text" placeholder="ne alacaksın?" />
+    </div>
+    <ul>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+    </ul>
+    <small class="mt-2 text-blue d-flex justify-content-end align-items-center"
+      >5 adet alınacak ürün vardır</small
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
