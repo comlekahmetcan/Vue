9ed5619c6050bdad501d1a84c387d9ad8e687d8c
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-30 14:04:17 +0300

    Vuex module structure(wrong use)

---
+++ 4.hafta/1-vuex-modules/src/store/modules/taskmanager.js
@@ -0,0 +1,14 @@
+export default {
+  state: {
+    itemList: [],
+    userList: [],
+  },
+  mutations: {
+    setItem(state, item) {
+      state.itemList.push(item);
+    },
+  },
+  getters: {
+    _itemList: (state) => state.itemList,
+  },
+};
