bcb7e8eb233c002137d942acd559faa29ff622ad
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-30 11:39:44 +0300

    Vuex(access to the store through the component)

---
+++ 4.hafta/0-vuex-introduction/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>
+    {{$store.state.person}}
+    {{$store.state.permissions}}
+    <ul>
+      <li v-for="permissions in $store.state.permissions" :key="permissions">
+        {{permissions}}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="user in $store.state.userList" :key="user">
+        {{user}}
+      </li>
+    </ul>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    console.log(this.$store.state.person);
+    console.log(this.$store.state.theme);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 4.hafta/0-vuex-introduction/src/store.js
@@ -0,0 +1,24 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+    person: {
+      age: 25,
+      name: "AhmetCan",
+      lname: "Çömlek",
+    },
+    theme: "dark",
+    permissions: [1, 2, 3, 4, 5],
+    userList: [
+      "Gökhan",
+      "Tayfun",
+      "İlker",
+      "Ramazan",
+      "Defne",
+      "Kamil",
+      "Cemil",
+    ],
+  },
+});
+
+export default store;
