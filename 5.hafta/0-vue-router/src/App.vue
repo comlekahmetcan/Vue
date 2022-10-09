4177d3a87690ee26dd2913b53d3b01b42e084e0e
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-09 11:04:41 +0300

    Style operation on navbar

--- 5.hafta/0-vue-router/src/App.vue
+++ 5.hafta/0-vue-router/src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <router-link to="/"> Anasayfa </router-link>
-  <router-link to="/hakkimda"> Hakkımda </router-link>
-  <router-view></router-view>
+  <div class="container">
+    <router-link class="nav-link" active-class="active"  to="/"> Anasayfa </router-link>
+    <router-link class="nav-link" active-class="active" to="/hakkimda"> Hakkımda </router-link>
+    <router-view></router-view>
+  </div>
 </template>
+
+<style>
+.nav-link {
+  padding: 5px 10px;
+  text-decoration: none;
+  border: 1px solid #fa6558;
+  color: #fa6658;
+  display: inline-block;
+  margin-right: 5px;
+  margin-top: 5px !important;
+}
+
+.active {
+  background-color: #fa6558;
+  color: #fff;
+}
+</style>
