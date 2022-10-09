1595b034bd49ffc8264191b51812929800d41963
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-09 11:31:00 +0300

    navigation via javascript

--- 5.hafta/0-vue-router/src/views/MyDetails.vue
+++ 5.hafta/0-vue-router/src/views/MyDetails.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>Details</h1>
+  <h1>{{ $route.params.userID }}</h1>
   <p>Açıklamalar burada yer alacaktır..</p>
+  <!-- <p>{{ $route.params.userID }}</p> -->
+  <button @click="goBack">Geri Dön</button>
 </template>
 
 <script>
 export default {
   created() {
     console.log(this.$route);
+  },
+  methods: {
+    goBack() {
+      this.$route.push("/");
+    },
   },
 };
 </script>
