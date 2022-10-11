ac503c5f6f7db53b8b4f0ebf70cc0afccaf348b8
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-11 11:49:04 +0300

    integration of the theme and homepage(tema ve ana sayfanın entegrasyonu)

--- 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
+++ 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import "@/assets/style.css";
 
+import appHeader from "@/components/Shared/appHeader";
+
 const app = createApp(App);
+app.component("AppHeader", appHeader);
 app.use(router);
 app.mount("#app");
