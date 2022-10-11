031f2332a3f3476f720dc2d2f2642b7843ea359f
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-11 16:04:24 +0300

    axios revised

--- 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
+++ 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/style.css";
-
+import {appAxios} from "./utils/appAxios";
 import appHeader from "@/components/Shared/appHeader";
 import appBookmarkList from "@/components/Shared/appBookmarkList";
-import appAxios from "./utils/appAxios";
+
 const app = createApp(App);
 app.component("AppHeader", appHeader);
 app.component("appBookmarkList", appBookmarkList);
