c87241581761671ef9919a06edbeebe494003efb
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-11 14:10:11 +0300

    appBookmarkList component created

--- 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
+++ 6.hafta-socialmark-app/ders-6-socialmark-uygulamasi/src/main.js
@@ -4,8 +4,9 @@ import router from "./router";
 import "@/assets/style.css";
 
 import appHeader from "@/components/Shared/appHeader";
-
+import appBookmarkList from "@/components/Shared/appBookmarkList";
 const app = createApp(App);
 app.component("AppHeader", appHeader);
+app.component("appBookmarkList", appBookmarkList);
 app.use(router);
 app.mount("#app");
